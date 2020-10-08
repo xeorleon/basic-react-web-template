1264412423c6c9aa126a3f57574a1637ffbd61ea
xeorleon <xeorleon@gmail.com>
2020-10-08 22:11:28 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-import './App.css';
+
 import {Home} from "./Home";
 import {About} from "./About";
 import {Contact} from "./Contact";
