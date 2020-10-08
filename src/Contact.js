ee6c920a768cf517274eeec6b216a185ebef7b4a
xeorleon <xeorleon@gmail.com>
2020-10-08 22:08:41 +0300

    First Commit

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Us!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+        varius turpis. Pellentesque blandit velit a felis egestas convallis a
+        nec magna.
+      </p>
+    </div>
+  );
+};
---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h2>Hello World!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+        varius turpis. Pellentesque blandit velit a felis egestas convallis a
+        nec magna. Nullam porta dapibus elementum. Aliquam erat volutpat.
+        Maecenas vitae condimentum nibh. Suspendisse porta blandit aliquet. Ut
+        suscipit sem vel urna pharetra venenatis. Class aptent taciti sociosqu
+        ad litora torquent per conubia nostra, per inceptos himenaeos. Integer
+        id orci placerat, tincidunt est at, auctor lacus.
+      </p>
+      <p>
+        Praesent convallis, sem nec ultricies tristique, leo elit commodo magna,
+        in congue orci sem et eros. Ut euismod odio at nibh varius, a lacinia
+        diam scelerisque. Pellentesque in congue elit, eu rhoncus ex. Sed nibh
+        magna, tristique ut sem facilisis, efficitur commodo dui.
+      </p>
+    </div>
+  );
+};
